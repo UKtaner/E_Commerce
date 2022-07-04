b61b0b52878ddbd16d5fc5a18dd6da04a8953604
UKtaner <tanercakin@gmail.com>
2022-07-04 23:18:01 +0100

    first commit

--- src/test/java/e_commerce/PageClass.java
+++ src/test/java/e_commerce/PageClass.java
@@ -4,13 +4,7 @@ public class PageClass {
 
     public static void main(String[] args) {
         System.out.println(".........................");
-         System.out.println(".........................");
-        System.out.println(".........................");
-        System.out.println(".........................");
-        System.out.println(".........................");
-        System.out.println(".........................");
-        System.out.println(".........................");
-        System.out.println(".........................");
+
 
 
     }
